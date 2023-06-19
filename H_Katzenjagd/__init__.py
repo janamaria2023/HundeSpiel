03831fdd2b54cf94221fe94695d509390faa1084
janamaria2023 <jana.schmollinger@gmx.de>
2023-06-19 11:59:09 +0200

    aa

---
+++ H_Katzenjagd/__init__.py
@@ -0,0 +1,77 @@
+import pygame
+import random
+
+
+class Hund(pygame.sprite.Sprite):
+    def __init__(self, F_BREITE, F_HOEHE):
+        super().__init__()
+        self.F_BREITE = F_BREITE
+        self.F_HOEHE = F_HOEHE
+        self.image = pygame.image.load("hund.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.F_BREITE / 2, self.F_HOEHE / 2)
+        self.punkte = 0
+        self.leben = 7
+        
+    def update(self):
+         gedrueckt = pygame.key.get_pressed()
+         if gedrueckt[pygame.K_UP]:
+             self.rect.y -= 8
+         if gedrueckt[pygame.K_DOWN]:
+             self.rect.y += 8
+         if gedrueckt[pygame.K_LEFT]:
+             self.rect.x -= 8
+         if gedrueckt[pygame.K_RIGHT]:
+             self.rect.x += 8
+         self.rect.clamp_ip(pygame.Rect(0, 0, self.F_BREITE, self.F_HOEHE))
+         
+         
+class Zufallsobjekt(pygame.sprite.Sprite):
+        bilder_top = [pygame.image.load("knochen.png"),
+                    pygame.image.load("spielzeug.png"),
+                    pygame.image.load("wasser.png")]
+        
+        bilder_flop = [pygame.image.load("katze.png"),
+                    pygame.image.load("laerm.png"),
+                    pygame.image.load("kinder.png")]
+        
+        def __init__(self, F_BREITE, F_HOEHE):
+            super().__init__()
+            self.F_BREITE = F_BREITE
+            self.F_HOEHE = F_HOEHE
+            
+            self.gut = random.choice((True, False))
+            
+            if self.gut:
+                self.image = random.choice(Zufallsobjekt.bilder_top)
+            else:
+                self.image =random.choice(Zufallsobjekt.bilder_flop)
+            
+            self.rect = self.image.get_rect()
+            
+            self.rect.center = (random.randint(0, self.F_BREITE),
+                                random.randint(-self.F_HOEHE, -self.rect.height))
+            
+            self.x_speed = random.randint(-2, 2)
+            self.y_speed = random.randint(1, 5)
+            
+        def update(self):
+            if self.rect.top > self.F_HOEHE:
+                self.kill()
+            else:
+                self.rect.x += self.x_speed
+                self.rect.y += self.y_speed
+                if random.randint(0, 120) == 0:
+                    self.x_speed = random.randint(-2, 2)
+
+    
+def text(text, fenster, position, groesse):
+    font = pygame.font.SysFont('arial', groesse)
+    text = font.render(text, False, (0, 0, 0))
+    F_BREITE = text.get_rect().width
+    fenster.blit(text, (position[0] - (F_BREITE / 2), position[1]))
+
+                 
+              
+                 
+             
---
+++ H_main.py
@@ -0,0 +1,68 @@
+import os
+dir1=os.getcwd()
+print(dir1)
+import sys
+import pygame
+import H_Katzenjagd
+
+
+N_MIN_OBJEKTE = 10
+F_BREITE = 1000
+F_HOEHE = 600
+
+pygame.init()
+fenster = pygame.display.set_mode((F_BREITE, F_HOEHE))
+sprites = pygame.sprite.Group()
+hund = H_Katzenjagd.Hund(F_BREITE, F_HOEHE)
+sprites.add(hund)
+uhr = pygame.time.Clock()
+
+t_kollision_top = -100
+t_kollision_flop = -100
+
+while True:
+     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+     n_neue_objekte = N_MIN_OBJEKTE - len(sprites) + hund.punkte // 2
+     
+     for i in range(n_neue_objekte):
+         sprites.add(H_Katzenjagd.Zufallsobjekt(F_BREITE, F_HOEHE))
+     
+     for sprite in sprites:
+         
+         if sprite != hund and pygame.sprite.collide_rect(hund, sprite):
+             if sprite.gut:
+                 hund.punkte += 1
+                 t_kollision_top = pygame.time.get_ticks()
+             else:
+                 hund.leben -= 1
+                 t_kollision_flop = pygame.time.get_ticks()
+                 if hund.leben <= 0:
+                     fenster.fill((255, 255, 255))
+                     H_Katzenjagd.text("GAME OVER", fenster, (F_BREITE / 2, F_HOEHE / 2), 50)
+                     H_Katzenjagd.text(str(hund.punkte) + "punkte", fenster, (F_BREITE / 2, F_HOEHE / 2 + 60), 30)
+                     pygame.display.flip()
+                     pygame.time.wait(1000)
+                     pygame.quit()
+                     sys.exit()
+             sprite.kill()
+             
+         if pygame.time.get_ticks() - t_kollision_flop < 100:
+             fenster.fill((255, 0, 0))
+    
+         elif pygame.time.get_ticks() -t_kollision_top < 100:
+             fenster.fill((0,255, 0))
+         else:
+             fenster.fill((255, 255, 255))
+             
+         sprites.update()
+         sprites.draw(fenster)
+         
+         H_Katzenjagd.text("punkte: " + str(hund.punkte), fenster, (F_BREITE - 100, F_HOEHE -50), 30)
+         H_Katzenjagd.text("leben: " + str(hund.leben), fenster, (80, F_HOEHE - 50), 30)
+        
+         pygame.display.flip()
+         uhr.tick(30)
